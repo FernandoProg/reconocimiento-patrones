e267a9952d7089d79e0e194d892096bfdf18cb8f
GustavoSepul <93997009+GustavoSepul@users.noreply.github.com>
2023-05-11 13:14:00 -0400

    metodos lda y regresion lineal

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import linear_model
 
 df = pd.read_csv('log2.csv').to_numpy()
 # missing_values = df.isnull().sum()
@@ -10,11 +12,53 @@ caracteristicas = df[:,np.array([0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11])]
 Y = df[:,np.array([4])]
 X_train, X_test, y_train, y_test = train_test_split(caracteristicas, Y, test_size=0.15, random_state=27)
 
-# Refresion Logistica
+# Regresion Logistica
 Clasif = LogisticRegression()
 y_pred = Clasif.fit(X_train, y_train).predict(X_test)
 print('\n Matriz de confusion: col - realidad, filas - predicc\n')
 print(confusion_matrix(y_pred, y_test))
-print('\n Accuracy Reg_Lin: %.4f' % accuracy_score(y_pred, y_test ))
+print('\n Accuracy Reg_Log: %.4f' % accuracy_score(y_pred, y_test ))
 
 # LDA
+Clasif = LinearDiscriminantAnalysis()
+y_pred = Clasif.fit(X_train, y_train).predict(X_test)
+print('\n Matriz de confusion: col - realidad, filas - predicc\n')
+print(confusion_matrix(y_pred, y_test))
+print('\n Accuracy Reg_LDA: %.4f' % accuracy_score(y_pred, y_test ))
+
+# Regresion Lineal
+NumClases=4
+Y[Y == 'allow'] = 0
+Y[Y == 'drop'] = 1
+Y[Y == 'deny'] = 2
+Y[Y == 'reset-both'] =3
+y = np.array([])
+for i in Y:
+    y=np.append(y,i[0]).astype(int)
+y_codif=np.zeros([y.shape[0],NumClases])
+
+for n in range(0,y.shape[0]):
+    y_codif[n,y[n]]=1
+X_train, X_test, y_train, y_test = train_test_split(caracteristicas, y_codif, test_size=0.15, random_state=27)
+regr0 = linear_model.LinearRegression()
+regr1 = linear_model.LinearRegression()
+regr2 = linear_model.LinearRegression()
+regr3 = linear_model.LinearRegression()
+regr0.fit(X_train, y_train[:,0])
+regr1.fit(X_train, y_train[:,1])
+regr2.fit(X_train, y_train[:,2])
+regr3.fit(X_train, y_train[:,3])
+y_predict0= regr0.predict(X_test)
+y_predict1= regr1.predict(X_test)
+y_predict2= regr2.predict(X_test)
+y_predict3= regr3.predict(X_test)
+Y_regr=np.array([y_predict0, y_predict1, y_predict2, y_predict3])
+Y_predictor=np.argmax(Y_regr, axis=0)
+ytest_dec=np.zeros(y_test.shape[0])
+for n in range(0,y_test.shape[0]):
+    ytest_dec[n]=np.where(y_test[n,:]==1)[0][0]
+#matriz de confusion
+print('\n Matriz de confusion: col - realidad, filas - predicc\n')
+print(confusion_matrix(Y_predictor, ytest_dec))
+#precision - accuracy
+print('\n Accuracy Reg_Lin: %.4f' % accuracy_score(Y_predictor, ytest_dec))
