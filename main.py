f80ccf7cd9ed0f4365869c5dbe10412d1febe67e
Fernando Cabezas <51241715+FernandoProg@users.noreply.github.com>
2023-05-15 21:32:55 -0400

    Prueba distintos clasificadores

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import linear_model
+from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
 
 df = pd.read_csv('log2.csv').to_numpy()
 # missing_values = df.isnull().sum()
@@ -15,6 +18,7 @@ X_train, X_test, y_train, y_test = train_test_split(caracteristicas, Y, test_siz
 # Regresion Logistica
 Clasif = LogisticRegression()
 y_pred = Clasif.fit(X_train, y_train).predict(X_test)
+print('Regresion Logistica')
 print('\n Matriz de confusion: col - realidad, filas - predicc\n')
 print(confusion_matrix(y_pred, y_test))
 print('\n Accuracy Reg_Log: %.4f' % accuracy_score(y_pred, y_test ))
@@ -22,6 +26,7 @@ print('\n Accuracy Reg_Log: %.4f' % accuracy_score(y_pred, y_test ))
 # LDA
 Clasif = LinearDiscriminantAnalysis()
 y_pred = Clasif.fit(X_train, y_train).predict(X_test)
+print('LDA')
 print('\n Matriz de confusion: col - realidad, filas - predicc\n')
 print(confusion_matrix(y_pred, y_test))
 print('\n Accuracy Reg_LDA: %.4f' % accuracy_score(y_pred, y_test ))
@@ -58,7 +63,47 @@ ytest_dec=np.zeros(y_test.shape[0])
 for n in range(0,y_test.shape[0]):
     ytest_dec[n]=np.where(y_test[n,:]==1)[0][0]
 #matriz de confusion
+print('Regresion Lineal')
 print('\n Matriz de confusion: col - realidad, filas - predicc\n')
 print(confusion_matrix(Y_predictor, ytest_dec))
 #precision - accuracy
 print('\n Accuracy Reg_Lin: %.4f' % accuracy_score(Y_predictor, ytest_dec))
+
+# Perceptron
+Y = df[:,np.array([4])]
+X_train, X_test, y_train, y_test = train_test_split(caracteristicas, Y, test_size=0.15, random_state=27)
+Clasif = Perceptron()
+y_pred = Clasif.fit(X_train, y_train).predict(X_test)
+print('Perceptron')
+print('\n Matriz de confusion: col - realidad, filas - predicc\n')
+print(confusion_matrix(y_pred, y_test))
+#precision - accuracy
+print('\n Accuracy Reg_Lin: %.4f' % accuracy_score(y_pred, y_test ))
+
+# Metlin
+y_pred = Clasif.fit(X_train, y_train).predict(X_test)
+print('Metlin')
+print('\n Matriz de confusion: col - realidad, filas - predicc\n')
+print(confusion_matrix(y_pred, y_test))
+#precision - accuracy
+print('\n Accuracy Reg_Lin: %.4f' % accuracy_score(y_pred, y_test ))
+
+# Naive Bayes
+Clasif = GaussianNB()
+y_pred = Clasif.fit(X_train, y_train).predict(X_test)
+print('Naive Bayes')
+print('\n Matriz de confusion: col - realidad, filas - predicc\n')
+print(confusion_matrix(y_pred, y_test))
+#precision - accuracy
+print('\n Accuracy Reg_Lin: %.4f' % accuracy_score(y_pred, y_test ))
+
+# SVM no lineal
+# Clasif = svm.SVC(kernel="rbf", C=10)
+# Clasif = svm.SVC(kernel="rbf", gamma=0.2)
+Clasif = svm.SVC(kernel="rbf", C=10, gamma=0.2)
+y_pred = Clasif.fit(X_train, y_train).predict(X_test)
+print('SVM no lineal')
+print('\n Matriz de confusion: col - realidad, filas - predicc\n')
+print(confusion_matrix(y_pred, y_test))
+#precision - accuracy
+print('\n Accuracy Reg_Lin: %.4f' % accuracy_score(y_pred, y_test ))
